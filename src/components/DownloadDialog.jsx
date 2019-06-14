1feb014cd7a9f5cd7ae63c1d6bbb4874e4538696
Hugueds <hugo-pdf@hotmail.com>
2019-06-14 17:25:46 -0300

    add dialog for download

---
+++ src/components/DownloadDialog.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Dialog, DialogTitle } from '@material-ui/core';
+
+
+
+export default function DownloadDialog(props) {
+  
+    return (
+
+        <Dialog open={props.open} onClick={props.handleClose}>
+            <DialogTitle id="dialog-title">DOWNLOAD</DialogTitle>            
+        </Dialog>
+
+    )
+}
+
+
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent );
+  }
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+  
+  // Checks if should display install popup notification:
+  if (isIos() && !isInStandaloneMode()) {
+    this.setState({ showInstallMessage: true });
+  }
